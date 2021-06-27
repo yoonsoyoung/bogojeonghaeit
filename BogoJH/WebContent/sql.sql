ab06a6e01d3c1c46d749226e73c28952d79fe1d6
yoonsoyoung <32251962+yoonsoyoung@users.noreply.github.com>
2021-06-27 18:24:55 +0900

    Upload sql

---
+++ BogoJH/WebContent/sql.sql
@@ -0,0 +1,12 @@
+create table seatest03(
+	day varchar2(20),
+	category varchar2(20),
+	num	number(3),
+	
+	PRIMARY KEY (day)
+)
+
+select * from SEATEST03;
+
+insert into SEATEST03 values('2019-08-12','sea4',50);
+insert into SEATEST03 values('2019-08-13','sea5',813);
